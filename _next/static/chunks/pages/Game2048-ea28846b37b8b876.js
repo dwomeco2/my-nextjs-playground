56e88bf017de794a2102705a1fe72887850e9fdf
dwomeco2 <dwomeco2@users.noreply.github.com>
2023-04-23 04:11:01 +0000

    deploy: ccc4f15ad734a111df30d75d2e2d6dfeff98a61f

---
+++ _next/static/chunks/pages/Game2048-ea28846b37b8b876.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[568],{4915:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Game2048",function(){return u(3030)}])}},function(n){n.O(0,[392,914,705,30,774,888,179],function(){return n(n.s=4915)}),_N_E=n.O()}]);
