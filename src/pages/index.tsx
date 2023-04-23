2d8351194d455127b9478f36773a7e66af0cbe3e
kenny lam <kennylamwk@gmail.com>
2023-04-23 12:04:11 +0800

    head redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { useRouter } from "next/router"
-import { useEffect } from "react"
+import Head from "next/head"
 
 export default function Index() {
-	const router = useRouter()
-	useEffect(() => {
-		void router.push("/Game2048")
-	}, [router])
-
-	return <div></div>
+	return (
+		<div>
+			<Head>
+				<meta http-equiv="Refresh" content="0; url='https://dwomeco2.github.io/my-nextjs-playground/Game2048'" />
+			</Head>
+		</div>
+	)
 }
